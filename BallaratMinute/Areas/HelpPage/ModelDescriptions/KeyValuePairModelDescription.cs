68bc0643e9d732e67686e2946edfd5fbfb137cee
Pierce Andjelkovic <pierceandjelkovic@gmail.com>
2015-07-04 14:53:14 +1000

    Blank Azure WebAPI solution for Azure

---
+++ BallaratMinute/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace BallaratMinute.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ BallaratMinute/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BallaratMinute.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
